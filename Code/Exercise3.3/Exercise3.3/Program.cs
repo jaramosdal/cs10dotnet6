5551da3fe93ae84f5998c1f2a22b9be8348da077
Javier Ramos Nodal <javiramos@soliss.com>
2022-03-08 21:10:26 +0100

    Creo BlazorServer desde el principio

---
+++ Code/Exercise3.3/Exercise3.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+for (int i = 0; i <= 100; i++)
+{
+    if (i%3==0 && i % 5 == 0)
+    {
+        Write("fizzbuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Write("fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Write("buzz");
+    }
+    else
+    {
+        Write(i);
+    }
+
+    if (i < 100)
+    {
+        Write(", ");
+    }
+}
--- vs4win/PracticalApps/Northwind.BlazorServer/Program.cs
+++ vs4win/PracticalApps/Northwind.BlazorServer/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Northwind.BlazorServer.Data;
-using Packt.Shared; // AddNorthwindContext extension method
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,17 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddNorthwindContext();
-builder.Services.AddTransient<INorthwindService, NorthwindService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-  app.UseExceptionHandler("/Error");
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -27,7 +24,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
