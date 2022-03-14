0af250a6ec1370b17a44cdeb8ea308a338c3e0aa
Javier Ramos Nodal <javiramosnodal@gmail.com>
2022-03-14 18:07:53 +0100

    BlazorWasm con HttpClient

---
+++ vs4win/PracticalApps/Northwind.BlazorWasm/Client/Data/INorthwindService.cs
@@ -0,0 +1,14 @@
+﻿using Packt.Shared;
+
+namespace Northwind.BlazorWasm.Client.Data
+{
+    public interface INorthwindService
+    {
+        Task<List<Customer>> GetCustomersAsync();
+        Task<List<Customer>> GetCustomersAsync(string country);
+        Task<Customer?> GetCustomerAsync(string id);
+        Task<Customer> CreateCustomerAsync(Customer customer);
+        Task<Customer> UpdateCustomerAsync(Customer customer);
+        Task DeleteCustomerAsync(string id);
+    }
+}
--- vs4win/PracticalApps/Northwind.BlazorWasm/Server/Controllers/CustomersController.cs
+++ vs4win/PracticalApps/Northwind.BlazorWasm/Server/Controllers/CustomersController.cs
@@ -1,20 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc; // [ApiController], [Route]
+using Microsoft.EntityFrameworkCore; // ToListAsync, FirstOrDefaultAsync
+using Packt.Shared; // NorthwindContext, Customer
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace Northwind.BlazorWasm.Server.Controllers;
 
-namespace Northwind.BlazorWasm.Server.Controllers
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController : ControllerBase
 {
-    public class CustomersController : Controller
+    private readonly NorthwindContext db;
+    public CustomersController(NorthwindContext db)
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        this.db = db;
+    }
+    [HttpGet]
+    public async Task<List<Customer>> GetCustomersAsync()
+    {
+        return await db.Customers.ToListAsync();
+    }
+    [HttpGet("in/{country}")] // different path to disambiguate
+    public async Task<List<Customer>> GetCustomersAsync(string country)
+    {
+        return await db.Customers
+          .Where(c => c.Country == country).ToListAsync();
+    }
+    [HttpGet("{id}")]
+    public async Task<Customer?> GetCustomerAsync(string id)
+    {
+        return await db.Customers
+          .FirstOrDefaultAsync(c => c.CustomerId == id);
+    }
+    [HttpPost]
+    public async Task<Customer?> CreateCustomerAsync
+      (Customer customerToAdd)
+    {
+        Customer? existing = await db.Customers.FirstOrDefaultAsync
+          (c => c.CustomerId == customerToAdd.CustomerId);
+        if (existing == null)
         {
-            return View();
+            db.Customers.Add(customerToAdd);
+            int affected = await db.SaveChangesAsync();
+            if (affected == 1)
+            {
+                return customerToAdd;
+            }
         }
+        return existing;
+    }
+    [HttpPut]
+    public async Task<Customer?> UpdateCustomerAsync(Customer c)
+    {
+        db.Entry(c).State = EntityState.Modified;
+        int affected = await db.SaveChangesAsync();
+        if (affected == 1)
+        {
+            return c;
+        }
+        return null;
+    }
+    [HttpDelete("{id}")]
+    public async Task<int> DeleteCustomerAsync(string id)
+    {
+        Customer? c = await db.Customers.FirstOrDefaultAsync
+          (c => c.CustomerId == id);
+        if (c != null)
+        {
+            db.Customers.Remove(c);
+            int affected = await db.SaveChangesAsync();
+            return affected;
+        }
+        return 0;
     }
 }
-
